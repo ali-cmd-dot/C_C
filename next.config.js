5e7025342bf1016cc3e14d09b037ed31d8f5eb34
ali-cmd-dot <ali@cautio.in>
2025-09-17 13:54:51 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,35 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    optimizeCss: true,
+  },
+  env: {
+    GOOGLE_SHEETS_API_KEY: 'AIzaSyACruF4Qmzod8c0UlwfsBZlujoKguKsFDM',
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+        ],
+      },
+    ];
+  },
+  poweredByHeader: false,
+}
+
+module.exports = nextConfig
